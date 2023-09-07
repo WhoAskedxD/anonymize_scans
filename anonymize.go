10e901346a59146add44a5b41a4812f2c4f9fd4b
WhoAskedxD <harryvan667@gmail.com>
2023-09-07 16:32:42 -0700

    add dicom info

--- anonymize.go
+++ anonymize.go
@@ -1,16 +1,28 @@
 package anonymize_scans
 
 import (
+	"fmt"
 	"io/fs"
+	"log"
+	"os"
 	"path/filepath"
+
+	"github.com/suyashkumar/dicom"
 )
 
-// grabs a directory as a type string and walks thru each file/folder given in that path.
-func GetFilePathsInSubfolders(directoryPath string) ([]string, error) {
+// searches through the provided folder and gives all the filepaths as a slice.
+func GetFilePathsInFolders(directoryPath string) ([]string, error) {
+	logFileName := "GetFilePathsInSubfolders.txt"
+	logger, logFile, err := createLogger(logFileName)
+	if err != nil {
+		fmt.Println("Error making log file for GetFilePathsInSubfolders", err)
+		return nil, err
+	}
+	defer logFile.Close()
+	logger.Println("Searching through path:", directoryPath)
 	var filePaths []string
-
 	// Walk through the directory and its subdirectories
-	err := filepath.WalkDir(directoryPath, func(path string, d fs.DirEntry, err error) error {
+	err = filepath.WalkDir(directoryPath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -25,6 +37,46 @@ func GetFilePathsInSubfolders(directoryPath string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	for _, paths := range filePaths {
+		logger.Println(paths)
+	}
 	return filePaths, nil
+}
+
+// checks to see if the filepath provided is a dicom file if so return meta data.
+func DicomInfoGrabber(dicomFilePath string) (map[string]string, error) {
+	logFileName := "DicomInfoGrabber.txt"
+	logger, logFile, err := createLogger(logFileName)
+	if err != nil {
+		fmt.Println("Error making log file for DicomInfoGrabber:", err)
+		return nil, err
+	}
+	defer logFile.Close()
+
+	logger.Println("checking if :", dicomFilePath, " is a valid Dicom..")
+	dicomInfo := make(map[string]string)
+	dataset, err := dicom.ParseFile(dicomFilePath, nil)
+	if err != nil {
+		logger.Println("Error parsing :", dicomFilePath)
+		return nil, err
+	}
+	for iter := dataset.FlatStatefulIterator(); iter.HasNext(); {
+		element := iter.Next()
+		dicomInfo[element.Tag.String()] = element.Value.String()
+	}
+	logger.Println("dicomInfo for :", dicomFilePath, " is :", dicomInfo)
+	return dicomInfo, nil
+}
+
+func createLogger(logFileName string) (*log.Logger, *os.File, error) {
+	// Create or open the log file
+	logFile, err := os.Create(logFileName)
+	if err != nil {
+		return nil, nil, err
+	}
+	// Create a logger that writes to the log file
+	logger := log.New(logFile, "", log.LstdFlags)
+	// Redirect standard output to the logger
+	log.SetOutput(logFile)
+	return logger, logFile, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/WhoAskedxD/anonymize_scans
 
 go 1.20
+
+require github.com/suyashkumar/dicom v1.0.6
+
+require golang.org/x/text v0.3.8 // indirect
